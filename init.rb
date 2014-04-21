98820546d2d93e015c7ee1b5f963c10451894007
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-04-21 17:56:43 +0200

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_wiki_issue_details do
+  name 'Redmine Wiki Issue Details plugin'
+  author 'Jean-Baptiste BARTH'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/jbbarth/redmine_wiki_issue_details'
+  author_url 'jeanbaptiste.barth@gmail.com'
+end
